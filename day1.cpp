e20ccb38b6e77ae360fdf1fce66c51079c48f30f
boessee <maryianezari@gmail.com>
2024-12-01 18:43:35 +0100

    First day, task 1

---
+++ day1.cpp
@@ -0,0 +1,5 @@
+//DAY1
+
+/*so we should re-locate the numbers in each group -> find the difference -> count solution
+to relocate easier add new first line each time and exclude the number from the original list - will be from biggest to smallest
+if 2 identical numbers - relocate them both*/ 
